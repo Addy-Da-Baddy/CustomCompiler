e415af359d5e92928d364fbaccb02b8884eb4022
Addy <das.adriteyo@gmail.com>
2024-12-11 19:48:22 +0530

    Update Parser

--- parser.c
+++ parser.c
@@ -4,33 +4,37 @@
 #include <ctype.h>
 #include "lexer.h"
 
-typedef struct{
-    string
-}NodeData;
-
 typedef struct Node{
-    Token token;
+    TokenType type;
+    char* value;
     struct Node* right;
     struct Node* left;
 }  Node;
 
 
-Node* createNode(Token token){
+/* Node* createNode(TokenType type){
     Node* newNode = (Node*)malloc(sizeof(Node));
     if (!newNode){
         printf("MemLoc failiure. Retry. \n");
         exit(1);
     }
-    newNode->token = token;
+    char* value = NULL;
+    newNode->type = type;
+    if (value){
+        newNode->value = value;
+    }
+    else {
+        newNode->value = NULL;
+    }
     newNode->right = NULL;
     newNode->left = NULL;
-}
+}*/
 
-void print_tree(Node* root){
+void printAST(Node* root){
     if (root==NULL)
         return;
     print_tree(root->left);
-    printf("%d \n: ", root->value);
+    printf("Node: Type = %d, Value = %s\n", root->type, root->value ? root->value : "NULL");
     print_tree(root->right);
 }
 Token *parser(Token* tokens){
@@ -38,9 +42,12 @@ Token *parser(Token* tokens){
     Node root ;
     Node left;
     Node right;
-    root.value = 10;
-    left.value = 20;
-    right.value = 30;
+    root.type = STRING;
+    root.value = "Hello";
+    left.type = KEYWORD;
+    left.value = "print";
+    right.type = INT;
+    right.value = "5";
     root.left = &left;
     root.right = &right;
     left.right = left.left = right.right = right.left = NULL;
