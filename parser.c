1379f67f1c5cc131a672a5ed559911dcc3984c78
Addy <das.adriteyo@gmail.com>
2024-12-24 17:48:12 +0530

    Updated prelim parser to work on exit(1)

--- parser.c
+++ parser.c
@@ -47,9 +47,9 @@ Node* createNode(TokenType type, char* value){
 void printAST(Node* root){
     if (root==NULL)
         return;
-    printAST(root->left);
     char* typeS = tokenString(root->type);
     printf("Node: Type = %s, Value = %s\n", typeS, root->value ? root->value : "NULL");
+    printAST(root->left);
     printAST(root->right);
 }
 
@@ -74,7 +74,42 @@ Token *parser(Token* tokens){
     }
     else {
         Node* open_paren_node = createNode(current_token->type, "(");
-        root->right = open_paren_node;
+        root->left = open_paren_node;
+        current_token++;
+    }
+    if (current_token->type == INT){
+        int x = current_token->intValue;
+        char* strVal = (char*)malloc(100);
+        sprintf(strVal, "%d", x);
+        Node* numNode = createNode(current_token->type, strVal);
+        root->left->left = numNode;
+        current_token++;
+    } else if (current_token->type == IDENTIFIER){
+        Node* idNode = createNode(current_token->type, current_token->stringValue);
+        root->left->left = idNode;
+        current_token++;
+    }
+    else {
+        printf("Expected identifier or integer \n");
+        exit(1);
+    }
+      if (current_token->type!=SEPARATOR || current_token->separatorValue != ')'){
+        printf("Expected ( \n ");
+        exit(1);
+    }
+    else {
+        Node* close_paren_node = createNode(current_token->type, ")");
+        root->left->left->right = close_paren_node;
+        current_token++;
+    }
+    if (current_token->type!=SEPARATOR || current_token->separatorValue!=';'){
+        printf("Expected ; \n");
+        exit(1);
+    }
+    else {
+        Node* semi_node = createNode(current_token->type, ";");
+        root->right = semi_node;
+        current_token++;
     }
     printAST(root);
 }
